a6b572ed42c3e078521d7528a9d4281c1322355c
Danilo Mello <danilodemellow@gmail.com>
2025-04-27 00:59:32 -0300

    chore: update vite config for deploy to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'node:path'
+import path from 'node:path';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://daniiomellogames.netlify.app',
   server: {
     open: true,
-    port: 3000
+    port: 3000,
   },
   resolve: {
     alias: {
       '@views': path.resolve(__dirname, 'src', 'views'),
       '@app': path.resolve(__dirname, 'src', 'app'),
-    }
-  }
-})
+    },
+  },
+});
