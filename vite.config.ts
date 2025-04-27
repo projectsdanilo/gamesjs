f9e8090e6452e7e3c03cfa33f9eb331f5c8b5d0d
Danilo Mello <danilodemellow@gmail.com>
2025-04-27 01:06:34 -0300

    chore: update vite config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://daniiomellogames.netlify.app',
+  base: 'https://daniilogamesjs.netlify.app',
   server: {
     open: true,
     port: 3000,
